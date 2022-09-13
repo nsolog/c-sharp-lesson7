291b055d9473bf62c5ed088b7d77c4280cd334b1
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-09-13 12:15:26 +0300

    Task resolved

---
+++ task3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = new int[5,5];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i,j]} ");
+        }
+        
+        Console.WriteLine();
+    }
+}
+
+void GetColumnAverage(int[,] arr)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        float sum = 0;
+        int counter = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum += arr[j, i];
+            counter++;
+        }
+
+        float average = sum / counter;
+        Console.Write($"{average}; ");
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+GetColumnAverage(array);
